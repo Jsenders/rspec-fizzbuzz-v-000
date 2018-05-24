e50e9aba885b16ea5fff291eecb8c48dae2c573f
Joseph Senders <jasenders@gmail.com>
2018-05-24 20:08:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 require "pry"
 def fizzbuzz(int = nil)
-if fizzbuzz(int) % 3 == 0
+if int % 3 == 0
   "Fizz"
-elsif fizzbuzz(int) % 5 == 0
+elsif int % 5 == 0
   "Buzz"
-elsif fizzbuzz(int) % 3 == 0 && 5 == 0
+elsif int % 3 == 0 && 5 == 0
   "Fizzbuzz"
 end
 end
