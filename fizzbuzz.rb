ad6f5bb157e85bfbbb315197f779fb3708f9d4f6
Joseph Senders <jasenders@gmail.com>
2018-05-24 20:24:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int = nil)
-if int % 3 == 0
-  "Fizz"
+if int % 3 == 0 && 5 == 0
+  "Fizzbuzz"
 elsif int % 5 == 0
   "Buzz"
-elsif int % 3 == 0 && 5 == 0
-  "Fizzbuzz"
+elsif int % 3 == 0 
+  "Fizz"
 end
 end
